2fa26f99246432504b2c5916f5a12d559d938ef2
takaiyuk <takaishikawa1225@gmail.com>
2018-10-23 17:22:35 +0900

    first commit

---
+++ one_hot_encoder.py
@@ -0,0 +1,9 @@
+## https://www.kaggle.com/jsaguiar/lightgbm-with-simple-features?scriptVersionId=6025993
+
+# One-hot encoding for categorical columns with get_dummies
+def one_hot_encoder(df, nan_as_category = True):
+    original_columns = list(df.columns)
+    categorical_columns = [col for col in df.columns if df[col].dtype == 'object']
+    df = pd.get_dummies(df, columns= categorical_columns, dummy_na= nan_as_category)
+    new_columns = [c for c in df.columns if c not in original_columns]
+    return df, new_columns
