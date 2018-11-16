76ac87f831352a723b7de23a56632c830c6eddb4
Taka Ishikawa <32270184+takaiyuk@users.noreply.github.com>
2018-11-16 23:18:41 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,20 +1,9 @@
 """
-Your rank will rise at least {num_team_onshot_zero}, if you are not lazy.
+Your rank on Public LB may be {num_team_onshot_zero} place higher than it is, if you are not lazy.
 
 From public leader board, we can get the number of the teams who submit only one submission, whose score is zero.
 The number is {num_team_onshot_zero} at the time I post this.
-So, I think Your rank will rise at least {num_team_onshot_zero} because {num_team_onshot_zero} team will shake down due to their wrong submission, if you are not in those teams.
-Is this prediction wrong?
-
-BTW, I used the code below.
-~~~```
-import pandas as pd
-data = pd.read_csv("ga-customer-revenue-prediction-publicleaderboard.csv")
-data_agg = data.groupby("TeamName").agg({"Score":["min", "count"]})
-num_team_onshot_zero = data_agg[(data_agg["Score"]["min"] == 0) & (data_agg["Score"]["count"]==1)].shape[0]
-print("The number of the teams who submit only one submission, whose score is zero:", num_team_onshot_zero)
-```~~~
-
-LB の Raw Data ダウンロードは、最高記録のみ書き込まれている。
-つまり、はじめに 0.0000 を叩き出したユーザーは、その後いくら Submit しても、1行しか抽出されない。・・・人力で数えるしかない。。
+This means that {num_team_onshot_zero} teams will disappear or shake down due to their wrong submission when Private LB is calculated.
+So you are now ranked {num_team_onshot_zero} place higher than it is, if you are not in those teams.
+Is this idea wrong?
 """
