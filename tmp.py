2061a07c246a399757472c4b6febf09a300d35ec
Taka Ishikawa <32270184+takaiyuk@users.noreply.github.com>
2018-11-16 14:42:07 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,5 +1,23 @@
-import pandas
+import pandas as pd
 data = pd.read_csv("ga-customer-revenue-prediction-publicleaderboard.csv")
 data_agg = data.groupby("TeamName").agg({"Score":["min", "count"]})
-num_team_onshot_zero) = data_agg[(data_agg["Score"]["min"] == 0) & (data_agg["Score"]["count"]==1)].shape[0]
+num_team_onshot_zero = data_agg[(data_agg["Score"]["min"] == 0) & (data_agg["Score"]["count"]==1)].shape[0]
 print("The number of the teams who submit only one submission, whose score is zero:", num_team_onshot_zero)
+
+"""
+Your rank will rise at least {num_team_onshot_zero}, if you are not lazy.
+
+From public leader board, we can get the number of the teams who submit only one submission, whose score is zero.
+The number is {num_team_onshot_zero} at the time I post this.
+So, I think Your rank will rise at least {num_team_onshot_zero} because {num_team_onshot_zero} team will shake down due to their wrong submission, if you are not in those teams.
+Is this prediction wrong?
+
+BTW, I used the code below.
+```
+import pandas as pd
+data = pd.read_csv("ga-customer-revenue-prediction-publicleaderboard.csv")
+data_agg = data.groupby("TeamName").agg({"Score":["min", "count"]})
+num_team_onshot_zero = data_agg[(data_agg["Score"]["min"] == 0) & (data_agg["Score"]["count"]==1)].shape[0]
+print("The number of the teams who submit only one submission, whose score is zero:", num_team_onshot_zero)
+```
+"""
