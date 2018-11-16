d072fed720d3d44e55cbd6af8d07e743dd7e997f
Taka Ishikawa <32270184+takaiyuk@users.noreply.github.com>
2018-11-16 15:39:16 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,9 +1,3 @@
-import pandas as pd
-data = pd.read_csv("ga-customer-revenue-prediction-publicleaderboard.csv")
-data_agg = data.groupby("TeamName").agg({"Score":["min", "count"]})
-num_team_onshot_zero = data_agg[(data_agg["Score"]["min"] == 0) & (data_agg["Score"]["count"]==1)].shape[0]
-print("The number of the teams who submit only one submission, whose score is zero:", num_team_onshot_zero)
-
 """
 Your rank will rise at least {num_team_onshot_zero}, if you are not lazy.
 
@@ -13,11 +7,14 @@ So, I think Your rank will rise at least {num_team_onshot_zero} because {num_tea
 Is this prediction wrong?
 
 BTW, I used the code below.
-```
+~~~```
 import pandas as pd
 data = pd.read_csv("ga-customer-revenue-prediction-publicleaderboard.csv")
 data_agg = data.groupby("TeamName").agg({"Score":["min", "count"]})
 num_team_onshot_zero = data_agg[(data_agg["Score"]["min"] == 0) & (data_agg["Score"]["count"]==1)].shape[0]
 print("The number of the teams who submit only one submission, whose score is zero:", num_team_onshot_zero)
-```
+```~~~
+
+LB の Raw Data ダウンロードは、最高記録のみ書き込まれている。
+つまり、はじめに 0.0000 を叩き出したユーザーは、その後いくら Submit しても、1行しか抽出されない。・・・人力で数えるしかない。。
 """
