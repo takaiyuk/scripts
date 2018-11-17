565f4aa5692fdd0da908958ebd53f3d985f8d0bc
Taka Ishikawa <32270184+takaiyuk@users.noreply.github.com>
2018-11-17 09:00:22 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,9 +1,3 @@
 """
-Your rank on Public LB may be {num_team_onshot_zero} place higher than it is, if you are not lazy.
-
-From public leader board, we can get the number of the teams who submit only one submission, whose score is zero.
-The number is {num_team_onshot_zero} at the time I post this.
-This means that {num_team_onshot_zero} teams will disappear or shake down due to their wrong submission when Private LB is calculated.
-So you are now ranked {num_team_onshot_zero} place higher than it is, if you are not in those teams.
-Is this idea wrong?
+This is tmp script.
 """
