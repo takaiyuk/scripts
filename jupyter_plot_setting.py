3065348c1062c120ca2f1f7ac698179d4ae5a613
Taka Ishikawa <32270184+takaiyuk@users.noreply.github.com>
2018-11-19 09:07:46 +0900

    Update jupyter_plot_setting.py

--- jupyter_plot_setting.py
+++ jupyter_plot_setting.py
@@ -1,6 +1,7 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
-plt.rcParams['font.family'] = 'IPAexGothic'  # if using Japanese on the plot
+sns.set()
 plt.style.use('dark_background')  # if background is dark theme
+plt.rcParams['font.family'] = 'IPAexGothic'  # if using Japanese on the plot
 %matplotlib inline
