54b649f0b343e5cebed0d5423f463c2c965fc29b
Taka Ishikawa <32270184+takaiyuk@users.noreply.github.com>
2018-12-06 12:32:50 +0900

    Update contextmanager_timer.py

--- contextmanager_timer.py
+++ contextmanager_timer.py
@@ -1,29 +1,23 @@
 import time
 from contextlib import contextmanager
 
-
 @contextmanager
 def timer(name):
     t0 = time.time()
     yield
     print(f'[{name}] done in {time.time() - t0:.0f} s')
 
-
 def load_data():
     return
-
 
 def preprocess():
     return
 
-
 def feature_engineering():
     return
 
-
 def modeling():
     return
-
 
 def main(): 
     with timer('process loading'):
